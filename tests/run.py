6320120cfc9b097234646a905217e6d4b653c8f1
Yuri Arbeletche <ysba000@gmail.com>
2023-01-13 22:25:54 -0300

    (tests) update run for basic flow

--- tests/run.py
+++ tests/run.py
@@ -3,8 +3,11 @@ from testbench import testbench_run
 
 circuit = maxpy.AxCircuit(top_name="adder4")#, synth_overwrite = True)
 circuit.set_testbench_script(testbench_run)
+circuit.rtl2py(target="exact")
+
 circuit.set_synth_tool("yosys")
-circuit.rtl2py(target="exact",log_opt=True, saif_opt=False)
+circuit.rtl2py(target="exact_yosys")
+
 
     #circuit.parameters = {
 #    "[[PARAM_K]]": ["1","2"],
