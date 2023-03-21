97d434094c4366832ba902b8d71e62f35b826a2c
Yuri Arbeletche <ysba000@gmail.com>
2023-03-21 13:31:37 -0300

    doc init

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Lumache'
+copyright = '2023, Graziella'
+author = 'Graziella'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [ 
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+#html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+# EPUB options
+epub_show_urls = 'footnote'
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+.. Lumache documentation master file, created by
+   sphinx-quickstart on Sat Mar 11 15:08:08 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Lumache's documentation!
+===================================
+
+**Lumache** (/lu'make/) is a Python library for cooks and food lovers that creates recipes mixing random ingredients. It pulls data from the `Open Food Facts database <https://world.openfoodfacts.org>`_ and offers a *simple* and *intuitive* API.
+
+Check out the :doc:`usage` section for further information, including how to :ref:`install <installation>` the project.
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+      usage
+      api
