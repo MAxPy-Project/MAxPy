04195dedd0c3e4fa7949f847257dcaad3ed875c6
Yuri Arbeletche <ysba000@gmail.com>
2023-03-30 20:21:05 -0300

    fix doc sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 import pathlib
 import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -23,6 +24,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ['_templates']
