ac5dd1d923c00f383c6412b907ab2c54ceb4d2fc
Yuri Arbeletche <ysba000@gmail.com>
2023-03-30 21:10:06 -0300

    fix doc build with action 6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,24 +1,60 @@
-# Configuration file for the Sphinx documentation builder.
+# # Configuration file for the Sphinx documentation builder.
+# #
+# # For the full list of built-in configuration values, see the documentation:
+# # https://www.sphinx-doc.org/en/master/usage/configuration.html
 #
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# import pathlib
+# import sys
+# sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+#
+# # -- Project information -----------------------------------------------------
+# # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+#
+# project = 'Lumache'
+# copyright = '2023, Graziella'
+# author = 'Graziella'
+# release = '0.1'
+#
+# # -- General configuration ---------------------------------------------------
+# # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+#
+# extensions = [
+#     'sphinx.ext.duration',
+#     'sphinx.ext.doctest',
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.autosummary',
+# ]
+#
+# templates_path = ['_templates']
+# exclude_patterns = []
+#
+#
+#
+# # -- Options for HTML output -------------------------------------------------
+# # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+#
+# html_theme = 'sphinx_rtd_theme'
+# html_static_path = []
 
-import pathlib
+
+import os
 import sys
-sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+from textwrap import dedent
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'Lumache'
 copyright = '2023, Graziella'
 author = 'Graziella'
 release = '0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+if os.environ.get('CI') is None:
+    ENV = 'lan'
+else:
+    ENV = 'wan'
+tags.add(ENV)
 
-extensions = [ 
+extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
@@ -27,11 +63,5 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
 html_static_path = []
