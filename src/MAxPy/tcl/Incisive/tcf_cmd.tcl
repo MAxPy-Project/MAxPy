5afd8e7a0ca73943a4b8b2452f8dc80f9e6a4493
ysba <ysba@users.noreply.github.com>
2023-03-31 00:10:45 +0000

    deploy: ac5dd1d923c00f383c6412b907ab2c54ceb4d2fc

--- src/MAxPy/tcl/Incisive/tcf_cmd.tcl
+++
@@ -1,3 +0,0 @@
-dumptcf -scope tst:DUT -output stimuli.tcf -internal -overwrite -verbose -flatformat -dumpwireasnet
-run
-dumptcf -end
--- tests/rtl/adder4.v
+++
@@ -1,6 +0,0 @@
-module adder4(A, B, SUM); 
-input  [3:0] A; 
-input  [3:0] B; 
-output [3:0] SUM;
-assign SUM = A + B; 
-endmodule
